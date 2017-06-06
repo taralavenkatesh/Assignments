4afc111522b3fe199ad31cac862e199b430c0b6f
n0216777 <n0216777@VDDP35X-XUJTOKB.lm.lmig.com>
2017-06-06 15:37:30 -0400

    Added Save Option

--- Assignment1/assignment_Mod_2.js
+++ Assignment1/assignment_Mod_2.js
@@ -1,14 +1,14 @@
 var mod2 = angular.module("mod2", []);
 mod2.service("myService", function () {
-    this.saveStudent = function($scope)
-    {
-    
-    var newItem ={};
-    newItem.name= $scope.name;
-    newItem.marks1=$scope.marks1;
-    newItem.marks2=$scope.marks2;
-    newItem.marks3=$scope.marks3;
+	this.saveStudent = function($scope)
+	{
 
-    return newItem;    
- }
- } );
+		var newItem ={};
+		newItem.name= $scope.name;
+		newItem.marks1=$scope.marks1;
+		newItem.marks2=$scope.marks2;
+		newItem.marks3=$scope.marks3;
+
+		return newItem;    
+	}
+} );
