cff4415deea89cefe72ca1460c3dcbe0855b72e7
Venkatesh Tarala <n0216777@VDDP35X-XUJTOKB.lm.lmig.com>
2017-06-22 15:06:08 -0400

    Changed to use promise

--- Assignment2/student_factory_mod.js
+++ Assignment2/student_factory_mod.js
@@ -13,7 +13,54 @@ studentFact.factory('studentFact', function($log, $http) {
 
 		return studentData;
 	}
-
 	return fact;
+});
+
+
+studentFact.factory('studentFactPromise', function($log,$q,$http) {
+	var fact2 = {};
+
+	fact2.getStudentByID = function(id) {        
+		var deferred = $q.defer();        
+		$http.get('http://localhost:3000/student/' + id)
+		.then(function(response){
+			$log.debug(response.data);
+			deferred.resolve(response.data);
+		})
+		.catch(function(response){
+			deferred.reject(response);
+		});
+		return deferred.promise;	
+	}
+
+
+	fact2.deleteStudentById = function(id) {        
+		var deferred = $q.defer();        
+		$http.delete('http://localhost:3000/student/' + id)
+		.then(function(response){
+			$log.debug(response.data);
+			deferred.resolve(response.data);
+		})
+		.catch(function(response){
+			deferred.reject(response);
+		});
+		return deferred.promise;	
+	}
+
+	fact2.updateStudent = function($scope) {        
+		var deferred = $q.defer();        
+		$http.put('http://localhost:3000/student/' + $scope.student.id,
+				$scope.student)
+				.then(function(response){
+					$log.debug(response.data);
+					deferred.resolve(response.data);
+				})
+				.catch(function(response){
+					deferred.reject(response);
+				});
+		return deferred.promise;	
+	}
+
+	return fact2;
 
 });
--- Assignment2/viewstudent.js
+++ Assignment2/viewstudent.js
@@ -1,6 +1,6 @@
 var viewStudentMod = angular.module("viewStudentMod", []);
 viewStudentMod.controller("viewStudentCtrl", function($scope, $http, $log,
-		studentFact) {
+		studentFact,studentFactPromise) {
 	$scope.status = "";
 	$scope.editForm = false;
 	$scope.editData = [];
@@ -9,35 +9,50 @@ viewStudentMod.controller("viewStudentCtrl", function($scope, $http, $log,
 		$scope.studentdata = response.data;
 	});
 
-	$scope.deleteStudent = function(id) {
-		var url1 = "http://localhost:3000/student/" + id;
-		$http({
-			method : "DELETE",
-			url : url1
-		}).then(function mySuccess(response) {
-			$log.debug(response.data);
-			$scope.status = "success";
-		}, function myError(response) {
-			$log.debug(response.data);
-			$scope.status = "unnable to delete";
-		});
-
+	/*
+	 * $scope.deleteStudent = function(id) { var url1 =
+	 * "http://localhost:3000/student/" + id; $http({ method : "DELETE", url :
+	 * url1 }).then(function mySuccess(response) { $log.debug(response.data);
+	 * $scope.status = "success"; }, function myError(response) {
+	 * $log.debug(response.data); $scope.status = "unnable to delete"; });
+	 *  }
+	 */
+	
+	$scope.deleteStudent = function(id) {			
+		$log.debug("From Promise");
+		studentFactPromise.deleteStudentById(id).then(function(){
+			$scope.status = "success";			 
+         })
+         .catch(function(response){
+                 console.log(response.status);
+                 $scope.status = "unnable to delete";
+         }); 
+		
 	}
 
-	$scope.editStudent = function(id) {
+	/*
+	 * $scope.editStudent = function(id) { $scope.status = ""; $scope.editForm =
+	 * true; $http.get('http://localhost:3000/student/' + id).then(
+	 * function(response) { $scope.student = response.data;
+	 * $log.debug($scope.student); }, function errorCallback(response) {
+	 * $log.debug("error"); }); }
+	 */
+	
+	
+	$scope.editStudent = function(id) {	
 		$scope.status = "";
 		$scope.editForm = true;
-		$http.get('http://localhost:3000/student/' + id).then(
-				function(response) {
-					$scope.student = response.data;
-					$log.debug($scope.student);
-				}, function errorCallback(response) {
-					$log.debug("error");
-				});
-
+		$log.debug("From Promise");
+		studentFactPromise.getStudentByID(id).then(function(data){
+			 $scope.student = data;
+         })
+         .catch(function(response){
+                 console.log(response.status);
+         }); 
+		
 	}
-
-	$scope.updateStudent = function() {
+	
+	/*$scope.updateStudent = function() {
 		if ($scope.studentform.$valid) {
 			$http.put('http://localhost:3000/student/' + $scope.student.id,
 					$scope.student).then(function successCallback(response) {
@@ -47,7 +62,25 @@ viewStudentMod.controller("viewStudentCtrl", function($scope, $http, $log,
 
 			$scope.editForm = false;
 		}
+	}
+	*/
+	
+	$scope.updateStudent = function() {
+		if ($scope.studentform.$valid) {
+			$log.debug("From Promise");
+			studentFactPromise.updateStudent($scope).then(function(data){
+				$scope.status = "success";
+	         })
+	         .catch(function(response){
+	                 console.log(response.status);
+	                 $scope.status = "Failed to Update";
+	         }); 
+			$scope.editForm = false;
+		}
 
 	}
+	
+	
+	
 
 });
